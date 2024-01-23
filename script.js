00169e5ce01df75af3018b805e25eeabcf7362b4
SvdNeo <sanjay.d@neoito.com>
2024-01-23 14:23:56 +0530

    created checkMessageButton

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
 const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
+checkMessageButton.addEventListener("click", () => {
+    if (messageInput.value === "") {
+      alert("Please enter a message.");
+      return;
+    }
+  
+    result.textContent = isSpam(messageInput.value)
+      ? "Oh no! This looks like a spam message."
+      : "This message does not seem to contain any spam.";
+    messageInput.value = "";
+  });
